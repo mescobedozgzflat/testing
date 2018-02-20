1e8739f5ee677240202d0e94343b385c2b7fb046
mescobedozgzflat <34891787+mescobedozgzflat@users.noreply.github.com>
2018-02-20 15:39:51 +0100

    Update config.rb

--- public/config.rb
+++ public/config.rb
@@ -1,4 +1,5 @@
 require 'compass/import-once/activate'
+require "autoprefixer-rails"
 
 project_path = File.expand_path(File.join(File.dirname(__FILE__)))
 
@@ -9,3 +10,22 @@ images_dir  = "images"
 
 relative_assets = true
 line_comments   = true
+
+on_stylesheet_saved do |css_file|
+  if top_level.environment == :development
+    # Run Autoprefixer
+    css = File.read(css_file)
+    File.open(css_file, 'w') do |io|
+      io << AutoprefixerRails.process(css, browsers: ['Android 2.3',
+            'Android >= 4',
+            'Chrome >= 20',
+            'Firefox >= 24', # Firefox 24 is the latest ESR
+            'Explorer >= 8',
+            'iOS >= 6',
+            'Opera >= 12',
+            'Safari >= 6'
+          ]
+        )
+    end
+  end
+end
